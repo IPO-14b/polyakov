e65bd6e0abe4e12a2b688dbe06b7a0052df9b8f1
Alrxander Polyakov <axelmen1994@mail.ru>
2017-06-15 05:09:30 +0300

    Добавление кода игры

---
+++ pong.js
@@ -0,0 +1,34 @@
+//=============================================================================
+// PONG
+//=============================================================================
+
+Pong = {
+
+  Defaults: {
+    width:        640,   // logical canvas width (browser will scale to physical canvas size - which is controlled by @media css queries)
+    height:       480,   // logical canvas height (ditto)
+    wallWidth:    12,
+    paddleWidth:  12,
+    paddleHeight: 60,
+    paddleSpeed:  2,     // should be able to cross court vertically   in 2 seconds
+    ballSpeed:    4,     // should be able to cross court horizontally in 4 seconds, at starting speed ...
+    ballAccel:    8,     // ... but accelerate as time passes
+    ballRadius:   5,
+    sound:        true
+  },
+
+  Colors: {
+    walls:           'white',
+    ball:            'white',
+    score:           'white',
+    footprint:       '#333',
+    predictionGuess: 'yellow',
+    predictionExact: 'red'
+  },
+
+  Images: [
+    "images/press1.png",
+    "images/press2.png",
+    "images/winner.png"
+  ]
+}
