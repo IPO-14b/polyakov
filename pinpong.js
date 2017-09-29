75bec9f757acc9ff3fdb978040e9d4bc37f96106
Alrxander Polyakov <axelmen1994@mail.ru>
2017-09-29 08:16:00 +0300

    nineteen

--- pinpong.js
+++ pinpong.js
@@ -89,3 +89,24 @@ window.requestAnimFrame = (function(){
                 ctx.fill();
             }
         };
+
+
+        // Start Button object
+        startBtn = {
+            w: 100,
+            h: 50,
+            x: W / 2 - 50,
+            y: H / 2 - 25,
+
+            draw: function() {
+                ctx.strokeStyle = "white";
+                ctx.lineWidth = "2";
+                ctx.strokeRect(this.x, this.y, this.w, this.h);
+
+                ctx.font = "18px Arial, sans-serif";
+                ctx.textAlign = "center";
+                ctx.textBaseline = "middle";
+                ctx.fillStlye = "white";
+                ctx.fillText("Старт", W / 2, H / 2);
+            }
+        };
